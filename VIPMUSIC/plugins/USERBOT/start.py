455a48db931ec87fb45de6a5d716c5ee5908c0c2
ArdNeriw <155285637+ArdNeriw@users.noreply.github.com>
2024-06-01 00:54:13 +0545

    Update start.py

--- VIPMUSIC/plugins/USERBOT/start.py
+++ VIPMUSIC/plugins/USERBOT/start.py
@@ -103,14 +103,14 @@ async def start_pm(client: Client, message: Message, _):
                 [
                     [
                         InlineKeyboardButton(
-                            text="📥 ᴠɪᴅᴇᴏ", callback_data=f"downloadvideo {query}"
+                            text="🐨 ᴠɪᴅᴇᴏ", callback_data=f"downloadvideo {query}"
                         ),
                         InlineKeyboardButton(
-                            text="📥 ᴀᴜᴅɪᴏ", callback_data=f"downloadaudio {query}"
+                            text="🐭 ᴀᴜᴅɪᴏ", callback_data=f"downloadaudio {query}"
                         ),
                     ],
                     [
-                        InlineKeyboardButton(text="🎧 sᴇᴇ ᴏɴ ʏᴏᴜᴛᴜʙᴇ 🎧", url=link),
+                        InlineKeyboardButton(text="🦊 sᴇᴇ ᴏɴ ʏᴏᴜᴛᴜʙᴇ 🦊", url=link),
                     ],
                 ]
             )
